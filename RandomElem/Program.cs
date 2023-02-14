152d3678f1e3bc1fc620b524585d8bb835cdd9f3
Aleksandr Utkin <utkin.aleksandr123@gmail.com>
2023-02-14 19:11:00 +0200

    Week6 code

---
+++ RandomElem/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] snacks = { "sushi", "pizza", "burger", "chicken wings", "asian takeaway" };
+
+Random rnd = new Random();
+
+int RandomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"Eat {snacks [RandomIndex]}");
